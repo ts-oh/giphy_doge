fd106869863d650c2ac41e47672af3bf6991628c
ts-oh <oh.timmms@gmail.com>
2022-03-19 19:28:49 +0700

    added try/catch block and err msg for dom

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ const loading = document.querySelector("#loading");
 const image = document.querySelector("#gif-image");
 
 async function doge() {
-  const response = await fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=eQwrIRtmL1edzZSCv155iDwpBliMnEb1&s=doge",
-    {
-      mode: "cors",
-    }
-  ).catch((err) => console.log(err));
-
-  const result = await response.json();
-  imgEl.childNodes[3].src = result.data.images.original.url;
-  imgEl.childNodes[3].alt = result.data.title;
-  imgEl.childNodes[3].onload = doneLoading();
+  try {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/translate?api_key=eQwrIRtmL1edzZSCv155iDwpBliMnEb1&s=doge",
+      {
+        mode: "cors",
+      }
+    );
+    const result = await response.json();
+    imgEl.childNodes[3].src = result.data.images.original.url;
+    imgEl.childNodes[3].alt = result.data.title;
+    imgEl.childNodes[3].onload = doneLoading();
+  } catch (error) {
+    console.log("Fetch error:" + " " + error);
+    errorOutput();
+  }
 }
 
 function loadingOutput() {
@@ -24,9 +28,12 @@ function loadingOutput() {
 function doneLoading() {
   loading.textContent = "";
 }
+
+function errorOutput() {
+  loading.textContent = "Error fetching data!!!";
+}
+
 btn.addEventListener("click", () => {
   loadingOutput();
   doge();
 });
-
-//image.onload(doneLoading());
