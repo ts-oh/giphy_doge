4a5f43736dccc78c03d207a71aa3ca84102d1c2b
ts-oh <oh.timmms@gmail.com>
2022-04-03 15:31:20 +0700

    format code with prettier and enforce rules with eslint

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
 const imgEl = document.querySelector("#img-container");
 const btn = document.querySelector("button");
 const loading = document.querySelector("#loading");
-const image = document.querySelector("#gif-image");
-
-async function doge() {
-  try {
-    const response = await fetch(
-      "https://api.giphy.com/v1/gifs/translate?api_key=eQwrIRtmL1edzZSCv155iDwpBliMnEb1&s=doge",
-      {
-        mode: "cors",
-      }
-    );
-    const result = await response.json();
-    imgEl.childNodes[3].src = result.data.images.original.url;
-    imgEl.childNodes[3].alt = result.data.title;
-    imgEl.childNodes[3].onload = doneLoading();
-  } catch (error) {
-    console.log("Fetch error:" + " " + error);
-    errorOutput();
-  }
-}
 
 function loadingOutput() {
   loading.textContent = "fetching doge gif...";
@@ -31,6 +12,23 @@ function doneLoading() {
 
 function errorOutput() {
   loading.textContent = "Error fetching data!!!";
+}
+
+async function doge() {
+  try {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/translate?api_key=BirTwq2uwFKufaHchWxto6oZwXlGyhdM&s=doge",
+      {
+        mode: "cors",
+      }
+    );
+    const result = await response.json();
+    imgEl.childNodes[3].src = result.data.images.original.url;
+    imgEl.childNodes[3].alt = result.data.title;
+    imgEl.childNodes[3].onload = doneLoading();
+  } catch (error) {
+    errorOutput();
+  }
 }
 
 btn.addEventListener("click", () => {
